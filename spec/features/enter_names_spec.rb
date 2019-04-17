079183a79e6cd1183931a26a2c2c653e5fcaa27f
Tara Sankhalpara <tsankhalpara@mac.com>
2019-04-17 12:44:54 +0100

    add web_helpers and create sign_in_and_play method, refactor code to use this method

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario "entering names" do
-    visit('/')
-    fill_in :player1_name, with: 'Tara'
-    fill_in :player2_name, with: 'Nandini'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Tara VS Nandini'
   end
 end
--- spec/features/points_spec.rb
+++ spec/features/points_spec.rb
@@ -1,8 +1,6 @@
 feature 'View points' do
   scenario "viewing player 2's points" do
-    visit('/')
-    fill_in :player2_name, with: 'Nandini'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Nandini: 100HP'
   end
 end
